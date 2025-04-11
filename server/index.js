a7aeb43f14393865277b7143898ac4f282cece48
Souvik <souvik13109@gmail.com>
2025-04-11 16:57:01 +0530

    Implement database connection and error handling; add constants for DB name

--- server/index.js
+++ server/index.js
@@ -1,14 +1,32 @@
 import express from 'express';
 const app = express();
-
-
 import dotenv from 'dotenv';
 dotenv.config({
-    path: './.env'
+    path:'./.env'
 });
-
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server running at:`, PORT);
 })
+
+// Database connection
+import connectDB from './src/db/index.js';
+connectDB();
+app.use(express.json())
+
+
+//error handling
+app.use((err, req, res, next) =>
+    {
+        const statusCode = err.statusCode ||500
+        const message= err.message || "Internal server error"
+    
+        return res.status(statusCode).json({
+            success:false,
+            statusCode,
+            message,
+        })
+        
+      
+    })
