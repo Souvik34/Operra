0d615cb18bae26d25a89c2022f6f7daa5b07c0e8
Souvik <souvik13109@gmail.com>
2025-04-12 03:39:56 +0530

    Refactor validation middleware and cleanup unused lines in validation schema

--- server/src/middlewares/validate.js
+++ server/src/middlewares/validate.js
@@ -1,7 +1,7 @@
 
 export const validate = (schema) => (req, res, next) => {
     try {
-      req.body = schema.parse(req.body); // parse throws if invalid
+      req.body = schema.parse(req.body);
       next();
     } catch (err) {
       return res.status(400).json({ success: false, errors: err.errors });
