55e27571a6666d465d02d005348f0913e1ded9ff
Souvik <souvik13109@gmail.com>
2025-04-13 13:30:18 +0530

    Boilerplate created

--- server/src/controllers/task.controller.js
+++ server/src/controllers/task.controller.js
@@ -1,20 +1,85 @@
 import { Task } from "../models/task.model.js";
 
-export const getTasks = async (req, res, next) => {}
+export const getTasks = async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        next(error);
+        
+    }
+}
 
-export const getTaskById = async (req, res, next) => {}
+export const getTaskById = async (req, res, next) => {
 
-export const createTask = async (req, res, next) => {}
+    try {
+        
+    } catch (error) {
+        next(error);
+        
+    }
+}
 
-export const updateTask = async (req, res, next) => {}
+export const createTask = async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        next(error);
+        
+    }
+}
 
-export const deleteTask = async (req, res, next) => {}
+export const updateTask = async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        next(error);
+        
+    }
+}
 
-export const updateTaskStatus = async (req, res, next) => {}
+export const deleteTask = async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        next(error);
+        
+    }
+}
 
-export const updateTaskList = async (req, res, next) => {}
+export const updateTaskStatus = async (req, res, next) => {
 
-export const getDashboardData = async (req, res, next) => {}
+    try {
+        
+    } catch (error) {
+        next(error);
+        
+    }
+}
 
-export const getUserDashboardData = async (req, res, next) => {}
+export const updateTaskList = async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        next(error);
+        
+    }
+}
+
+export const getDashboardData = async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        next(error);
+        
+    }
+}
+
+export const getUserDashboardData = async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        next(error);
+        
+    }
+}
 
