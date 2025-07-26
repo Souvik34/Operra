881af1bb8117ceb53637b15d052d0d0b7099de3b
Souvik <souvik13109@gmail.com>
2025-07-26 21:06:14 +0530

    Add Dashboard and DashboardLayout components

---
+++ client/src/pages/Admin/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import DashboardLayout from '../../components/layouts/DashboardLayout'
+
+const Dashboard = () => {
+  return (
+    <DashboardLayout>Dashboard</DashboardLayout>
+  )
+}
+
+export default Dashboard
